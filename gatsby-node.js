91136eca97b0a9b539ff9589d268dc45ebecf096
padawanR0k <padawanR0k@gmail.com>
2022-11-06 16:24:12 +0900

    up: 개츠지 설정파일 수정

--- gatsby-node.js
+++ gatsby-node.js
@@ -165,7 +165,7 @@ exports.createPages = async ({ graphql, actions }) => {
       // template.
       //
       // Note that the template has to exist first, or else the build will fail.
-      component: path.resolve(`./src/templates/${layout || 'post'}.tsx`),
+      component: path.resolve(`./src/templates/${layout || 'Post'}.tsx`),
       context: {
         // Data passed to context is available in page queries as GraphQL variables.
         slug,
