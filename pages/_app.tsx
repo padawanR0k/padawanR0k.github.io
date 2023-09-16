cc0fb9cfeaa9949bd688bf31ad7ca682e53ddee7
padawanr0k <padawanr0k@gmail.com>
2023-09-16 19:04:30 +0900

    feat: ga 스크립트 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,35 @@
+import * as gtag from '../lib/gtag'
 import './styles.css'
+import {Fragment} from "react";
+import Script from 'next/script';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  gtag.useGtag();
+  return <Fragment>
+    <Component {...pageProps} />
+    {process.env.NODE_ENV !== 'development' && (
+        <>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <Script
+              strategy="afterInteractive"
+              src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+          />
+          <Script
+              id="gtag-init"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${gtag.GA_TRACKING_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+              }}
+          />
+        </>
+    )}
+  </Fragment>
 }
